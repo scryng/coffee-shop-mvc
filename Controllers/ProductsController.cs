03b2b873e649834eb5b273643d0aac90f5e139c6
scryng <scrynng@gmail.com>
2024-12-02 14:13:46 -0300

    :sparkles: feat: change return type of Index action from string to IActionResult and update return statement to call View()

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,9 +7,9 @@ public class ProductsController : Controller
 {
     // 
     // GET: /Products/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
     // 
     // GET: /Products/Welcome/ 
