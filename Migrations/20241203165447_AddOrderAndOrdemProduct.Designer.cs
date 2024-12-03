5afaf7fafd7618278d74012fa48360f2ae95c387
scryng <gnyrcs@gmail.com>
2024-12-03 13:56:43 -0300

    :sparkles: feat: add Order and OrderProduct entities to CoffeeShopContext, and update model snapshot

--- Migrations/20241203165447_AddOrderAndOrdemProduct.Designer.cs
+++ Migrations/20241203165447_AddOrderAndOrdemProduct.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using coffee_shop_mvc.Data;
 
@@ -10,9 +11,11 @@ using coffee_shop_mvc.Data;
 namespace coffee_shop_mvc.Migrations
 {
     [DbContext(typeof(CoffeeShopContext))]
-    partial class CoffeeShopContextModelSnapshot : ModelSnapshot
+    [Migration("20241203165447_AddOrderAndOrdemProduct")]
+    partial class AddOrderAndOrdemProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
